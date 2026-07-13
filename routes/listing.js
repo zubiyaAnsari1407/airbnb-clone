5b6a2037a4329da86cadb57cfa6b7b21ca31ead7
Zubiya Ansari <zubiyaansari417@gmail.com>
2026-07-14 02:51:16 +0530

    cloudinary-storage

--- routes/listing.js
+++ routes/listing.js
@@ -1,101 +1,46 @@
-  const express = require("express");
+const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const {listingSchema } = require("../schema.js");  //joi schema
 const Listing = require("../models/listing.js");
-const {isLoggedIn} = require("../middleware.js");
+const {isLoggedIn,isOwner,validateListing} = require("../middleware.js");
+const ListingController = require("../controller/listing.js"); //Controller
+const multer  = require('multer')
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
 
-//joi validation part for listing
-const validateListing = (req,res,next)=>{
-     let {error} = listingSchema.validate(req.body);
-       if(error){
-        let errMsg = error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-       }
-       else{
-        next();
-       }
-}
-
-
-//Index Route
-router.get("/", 
-    wrapAsync(async ( req,res) => {
-   const allListings =  await Listing.find({}); 
-   res.render("./listings/index.ejs", {allListings});
-}));
-
-//New Route 
-router.get("/new", isLoggedIn,
-     (req,res) =>{
-    
-    res.render("./listings/new.ejs")
+//router.route() to combine routes with same path and different methods(like get, post, put, delete) 
+// to make code cleaner and more readable
+router
+.route("/")
+.get(wrapAsync(ListingController.index))//Index Route
+// .post(validateListing,
+//     isLoggedIn,
+//     wrapAsync(ListingController.createListing)) //create route 
+.post(upload.single('listing[image][url]'),(req,res)=>{
+    res.send(req.file);
 });
+    
+//New Route 
+router.get("/new", isLoggedIn,ListingController.renderNewForm);
 
-//show route 
-router.get("/:id", 
-    wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id)
-    .populate("reviews")
-    .populate("owner");
-    if(!listing){
-        req.flash("error","Listing you requested for does not exist")
-        res.redirect("/listings")
-    }
-    console.log(listing);
-    res.render("./listings/show.ejs", { listing })
-}));
+router.route("/:id")
+.put( validateListing,
+      isLoggedIn,
+      isOwner,
+    wrapAsync(ListingController.updateListing)) //update route
+.get( wrapAsync(ListingController.showListing)) //show route
+.delete(isLoggedIn,
+     isOwner,
+    wrapAsync(ListingController.destroyListing)); //delete route
 
-//create route 
-router.post("/",
-    validateListing,
-    isLoggedIn,
-    wrapAsync( async (req, res,next) => {
-       const newListing = new Listing(req.body.listing);
-       newListing.owner = req.user._id;
-    await newListing.save();
-    req.flash("success","New Listing Created!");
-    res.redirect("/login");
 
-}));
 
 //Edit Route
-router.get("/:id/edit",isLoggedIn,
-     wrapAsync(async (req,res)=>{
-     let {id} = req.params;
-    const listing = await Listing.findById(id);
-     if(!listing){
-        req.flash("error","Listing you requested for does not exist")
-        res.redirect("/listings")
-    }
-    res.render("listings/edit.ejs",{ listing });
-}));
+router.get("/:id/edit",
+    isLoggedIn,
+    isOwner,
+     wrapAsync(ListingController.renderEditForm));
 
-//Update Route 
-router.put("/:id", 
-      validateListing,
-      isLoggedIn,
-    wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    let listing = await Listing.findById(id);
-    if(!listing.owner.equals(currUser._id)){
-             req.flash("error","You dont have permission to edit ");
 
-    }
-    res.redirect(`/listings/${ id }`);
-}));
-
-//delete route 
-router.delete('/:id', isLoggedIn,
-    wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-   let deletedListing = await Listing.findByIdAndDelete(id);
-   console.log(deletedListing);
-   req.flash("success","Listing Deleted! ");
-   res.redirect("/listings");
-}));
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -4,59 +4,24 @@ const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const {saveRedirectUrl} = require("../middleware.js");
-// sign Up get
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs");
-})
-// sign Up POST
-    router.post(
-        "/signup", 
-    wrapAsync(async(req,res)=>{
-    try{
-         let {username,email,password} = req.body;
-    const newUser = new User({email,username});
-    const registeredUser = await User.register(newUser,password); 
-    console.log(registeredUser);
-    req.login(registeredUser,(err)=>{
-        if (err){
-            return next(err);
-    }
-     req.flash("success","Welcome to WanderLust !");
-    res.redirect("/listings"); 
-})
-   
-    } catch(e){
-        req.flash("error",e.message);
-        res.redirect("/signup");
-    }
-}))
+const UserController = require("../controller/users.js");
 
-// Login GET
-router.get("/login",(req,res)=>{
-    res.render("users/login.ejs");
-});
+//signup route
+router.route("/signup")
+.get( UserController.renderSignupForm)
+.post( wrapAsync(UserController.signup))
 
-//Login POST
-router.post("/login", 
+//login route
+router.route("/login")
+.get(UserController.renderLoginForm)
+.post(
     saveRedirectUrl,
      passport.authenticate( "local", {
     failureRedirect:"/login",
     failureFlash: true,
-}),
-    async(req,res)=>{
-    req.flash("success","Welcome back to WanderLust");
-    let redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
-})
+}),UserController.login)
 
 //logout 
-router.get("/logout",(req,res,next)=>{
-    req.logout((err)=>{
-        if(err){
-            return next(err);
-        }
-        req.flash("success","You are Logged out!");
-        res.redirect("/listings");
-    })
-})
+router.get("/logout", UserController.logout);
+
 module.exports = router;
